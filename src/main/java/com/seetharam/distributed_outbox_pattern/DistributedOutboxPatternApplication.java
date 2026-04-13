e1dd0740dd741ef7ffda59a862594fc2416d343f
Seetha Ram Rao Vanam <seetharam2602@gmail.com>
2026-04-13 10:38:33 -0500

    Initial commit

---
+++ src/main/java/com/seetharam/distributed_outbox_pattern/DistributedOutboxPatternApplication.java
@@ -0,0 +1,13 @@
+package com.seetharam.distributed_outbox_pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedOutboxPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedOutboxPatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seetharam/distributed_outbox_pattern/DistributedOutboxPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seetharam.distributed_outbox_pattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedOutboxPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
